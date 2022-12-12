ff0e8b65d68afbcacc969a24598db05473faf2bd
Niki Papavasileiou <107496316+niki-papavasileiou@users.noreply.github.com>
2022-12-12 18:42:21 +0200

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -1,8 +1,8 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
+from mpl_toolkits import mplot3d
+
 pd.options.mode.chained_assignment = None
 
 df = pd.read_csv('High_MEO_FEDO.csv')
@@ -12,24 +12,24 @@ df = df.set_index('epoch')
 
 df['fedo_1'][df['fedo_1']< 0] = np.nan
 #df = df[df['fedo_1'].notna()]
+
 df['fedo_2'][df['fedo_2']< 0] = np.nan
 #df = df[df['fedo_2'].notna()]
+
 df['fedo_3'][df['fedo_3']< 0] = np.nan 
 df = df[df['fedo_3'].notna()]
 
+df = df.loc[(df['odi_unilib_l'] > 1 ) & (df['odi_unilib_l'] < 8) &(df['odi_unilib_alpha_eq']<90)  &(df['odi_unilib_alpha_eq']>0) ]
+
 startdate = pd.to_datetime("2006-01-10 23:50:13").date()
-enddate = pd.to_datetime("2006-01-13 12:07:53").date()
-df = df.loc[startdate:enddate]
+enddate = pd.to_datetime("2010-10-13 12:07:53").date()
+date= df.loc[startdate:enddate]
+l = date['odi_unilib_l']
 
-df1 = df.loc[(df['odi_unilib_l'] > 1 ) & (df['odi_unilib_l'] < 8) &(df['odi_unilib_alpha_eq']<90)  &(df['odi_unilib_alpha_eq']>0) ]
 
-#df1 = df1.groupby(['odi_unilib_l', 'epoch'],sort=False).agg(['mean'])
+date['epoch'] = date.index
+df_new = date.filter(['epoch','odi_unilib_l','fedo_3'], axis= 1 )
+fedo3= df_new['fedo_3'] 
 
-df2matrix = df1.pivot_table(index='odi_unilib_l', columns='epoch',values='fedo_3')
-
-t = sns.heatmap(df2matrix, cmap="crest",norm=LogNorm())
-plt.xlabel("epoch", size=14)
-plt.ylabel("L", size=14)
-plt.title("FEDO ", size=14)
-plt.tight_layout()
-plt.savefig('heatmap.jpg',dpi=150)
+df_new.plot(kind = 'scatter', x = 'epoch' , y = 'odi_unilib_l', c = np.log10(fedo3) ,colorbar = True)
+plt.show()
